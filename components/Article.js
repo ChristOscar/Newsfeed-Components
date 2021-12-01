7e81123fbe7c0b6382dc783162afd6cfcd9dd1bb
Christwide Oscar <triple101.games@gmail.com>
2021-12-01 16:31:19 -0600

    Move Data set back to article.js and finished the articles

--- components/Article.js
+++ components/Article.js
@@ -89,28 +89,89 @@ const data = [
   }
 ];
 
-/*
-  Step 1: Write a component called 'articleMaker' to create an article.
-  Your component is a function that takes an article object as its only argument,
-  and returns a DOM node looking like the one below:
 
-  <div class="article">
-    <h2>{title of the article}</h2>
-    <p class="date">{date of the article}</p>
 
-    {three separate paragraph elements}
+  // Step 1: Write a component called 'articleMaker' to create an article.
+  // Your component is a function that takes an article object as its only argument,
+  // and returns a DOM node looking like the one below:
 
-    <span class="expandButton">+</span>
-  </div>
+  // <div class="article">
+  //   <h2>{title of the article}</h2>
+  //   <p class="date">{date of the article}</p>
 
-  Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
-  This listener should toggle the class 'article-open' on div.article.
+  //   {three separate paragraph elements}
 
-  Step 3: Don't forget to return something from your function!
+  //   <span class="expandButton">+</span>
+  // </div>
 
-  Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
-  to create a div.article element and append it to the DOM inside div.articles (see index.html).
+function articleMaker(articleObj){
+  // Instantiate all the elements needed for a panel
+  const panel = document.createElement('div');
+  const panelTitle = document.createElement('h2');
+  const panelDate = document.createElement('p');
+  const panelContent1 = document.createElement('p');
+  const panelContent2 = document.createElement('p');
+  const panelContent3 = document.createElement('p');
+  const panelButton = document.createElement('span');
 
-  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
-  Refresh the page to see the new article.
-*/
+  // Setup the structure of our elements
+  panel.appendChild(panelTitle);
+  panel.appendChild(panelDate);
+  panel.appendChild(panelContent1);
+  panel.appendChild(panelContent2);
+  panel.appendChild(panelContent3);
+  panel.appendChild(panelButton);
+
+  // Making sure that computer knows what data it is looking for 
+  panelTitle.textContent = articleObj['title'];
+  panelDate.textContent = articleObj['date'];
+  panelContent1.textContent = articleObj['firstParagraph'];
+  panelContent2.textContent = articleObj['secondParagraph'];
+  panelContent3.textContent = articleObj['thirdParagraph'];
+
+  // <div class="article">
+  //   <h2>{title of the article}</h2>
+  //   <p class="date">{date of the article}</p>
+
+  //   {three separate paragraph elements}
+
+  //   <span class="expandButton">+</span>
+  // </div>
+
+  // Adding proper class names to our elments
+  panel.classList.add('article');
+  panelDate.classList.add('date');
+  panelButton.classList.add('expandButton');
+
+  panelButton.addEventListener('click', (c) => {
+    c.target.toggle('article-open');
+  });
+
+  console.log(panel);
+  return panel;
+
+}
+
+const articles = document.querySelector('.articles');
+console.log(articles);
+data.forEach(dataPoint =>{
+  const articleToAdd = articleMaker(dataPoint);
+  articles.appendChild(articleToAdd);
+
+});
+
+
+  // Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
+  // This listener should toggle the class 'article-open' on div.article.
+
+  // Step 3: Don't forget to return something from your function!
+
+  // Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
+  // to create a div.article element and append it to the DOM inside div.articles (see index.html).
+
+  // Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
+  // Refresh the page to see the new article.
+
+
+
+
